1e4300c06b9325ad5e29b89d12a06154008dc837
João Vitor Silva Maia <vitor.smaia1@gmail.com>
2022-09-24 19:16:49 -0300

    Commit

--- app/Http/Livewire/Post/ListCard.php
+++ app/Http/Livewire/Post/ListCard.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Post;
 
+use App\Models\Comment;
 use App\Models\Post;
 use Livewire\Component;
 use StdClass;
@@ -13,7 +14,57 @@ class ListCard extends Component
     ];
     public function posts()
     {
-        return Post::query()->with('comments')->orderBy('id', 'DESC')->get();
+//        return
+//        $post = Post::query();
+//        $post->join('comments','comments.id','posts.comment_id')
+//            ->join('users', 'users.id','comments.user_id')
+//            ->orderBy('posts.id', 'DESC')->get();
+
+        $post = Comment::query();
+        $post = $post->join('posts as p','p.user_id','comments.user_id')
+            ->join('users','users.id','p.user_id');
+        $post = $post->where('users.id', 1)
+
+            ->limit(1)
+            ->get();
+        dd($post);
+
+
+
+        $rolesDB = Role::query()
+            ->join('tenants as tenant','tenant.id','roles.tenant_id')
+            ->join('users as user','user.tenant_id', 'tenant.id')
+            ->orderBy('name', 'ASC');
+
+        if ($scope == 'Integrador' && isset($orderDB) && $orderDB) {
+            $rolesDB->where(function($query) use ($orderDB) {
+                $query->where('tenant.scope', 'Integrador')
+                    ->where('id',$orderDB->integrator_id)
+                    ->where('user.status', 'Ativo');
+            });
+        }
+
+        if (isset($scope) && !$orderDB) {
+            $rolesDB->withoutGlobalScope('tenant')->whereHas('tenant', function ($query) use ($scope) {
+                $query->where('scope', $scope);
+            });
+        }
+
+        if ($scope == 'Integrador' && isset($orderDB) && $orderDB) {
+            $rolesDB->withoutGlobalScope('tenant')->whereHas('tenant', function ($query) use ($orderDB) {
+                $query->where('scope', 'Integrador')
+                    ->where('id', $orderDB->integrator_id);
+            });
+        }
+
+        if ($scope == 'Distribuidor' && isset($orderDB) && $orderDB) {
+            $rolesDB->withoutGlobalScope('tenant')->whereHas('tenant', function ($query) use ($orderDB) {
+                $query->where('scope', 'Distribuidor')
+                    ->where('id', $orderDB->distributor_id);
+            });
+        }
+
+        $rolesDB = $rolesDB->get();
     }
 
     public function render()
